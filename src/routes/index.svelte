caf7ea0e6f55a869547099fcf147b668a2d77803
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2022-01-30 17:39:34 -0500

    - Added splashscreen - Removed page zoom - Removed text select

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 	import { gender } from '../stores/app-store';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
-	let splashscreenIsShowing = false;
+	let splashscreenIsShowing = true;
 	$: fieldsetDisabled = $firstname.length === 0;
 
 	function increment() {
@@ -24,68 +24,80 @@
 	}
 
 	onMount(async () => {
-		splashscreenIsShowing = true;
 		setTimeout(() => {
 			splashscreenIsShowing = false;
-		}, 1000);
+		}, 500);
 	});
 </script>
 
 {#if splashscreenIsShowing}
 	<div out:fade={{ duration: 500 }} class="splashscreen" />
+{:else}
+	<main>
+		<h1 class="header">Welcome to Activity</h1>
+
+		<button on:click={requestFullscreen}>Play in fullscreen</button>
+
+		<p>
+			This is using the game principles of the original Activity board game made in Germany. All
+			credits go to the original publisher.
+		</p>
+
+		{#if $firstname.length > 0}
+			<h2>Hello {$firstname}! {$gender === 'male' ? '👨🏼' : '👩🏼'}</h2>
+		{:else}
+			<h3>Hello stranger! What is your name?</h3>
+		{/if}
+
+		<input type="text" min="1" bind:value={$firstname} placeholder="Enter your firstname" />
+		<p />
+
+		<fieldset class="button-fieldset" disabled={fieldsetDisabled}>
+			<button on:click={decrement}>Dec number</button>
+			<button on:click={increment}>Inc number</button>
+			<div class="count-display">Number: {$count}</div>
+		</fieldset>
+
+		<fieldset class="button-fieldset" disabled={fieldsetDisabled}>
+			<input class="radio-button" type="radio" bind:group={$gender} value="male" id="male" />
+			<label for="male">👨🏼 Male</label>
+			<input class="radio-button" type="radio" bind:group={$gender} value="female" id="female" />
+			<label for="female">👩🏼 Female</label>
+		</fieldset>
+
+		{#each { length: 42 } as _, i}
+			<div class="list-element">{i + 1}</div>
+		{/each}
+	</main>
 {/if}
 
-<h1 class="header">Welcome to Activity</h1>
-
-<div class="scrollable">
-	<button on:click={requestFullscreen}>Play in fullscreen</button>
-
-	<p>
-		This is using the game principles of the original Activity board game made in Germany. All
-		credits go to the original publisher.
-	</p>
-
-	{#if $firstname.length > 0}
-		<h2>Hello {$firstname}! {$gender === 'male' ? '👨🏼' : '👩🏼'}</h2>
-	{:else}
-		<h3>Hello stranger! What is your name?</h3>
-	{/if}
-
-	<input type="text" min="1" bind:value={$firstname} placeholder="Enter your firstname" />
-	<p />
-
-	<fieldset class="button-fieldset" disabled={fieldsetDisabled}>
-		<button on:click={decrement}>Dec number</button>
-		<button on:click={increment}>Inc number</button>
-		<div class="count-display">Number: {$count}</div>
-	</fieldset>
-
-	<fieldset class="button-fieldset" disabled={fieldsetDisabled}>
-		<input class="radio-button" type="radio" bind:group={$gender} value="male" id="male" />
-		<label for="male">👨🏼 Male</label>
-		<input class="radio-button" type="radio" bind:group={$gender} value="female" id="female" />
-		<label for="female">👩🏼 Female</label>
-	</fieldset>
-
-	{#each { length: 100 } as _, i}
-		<div class="list-element">{i + 1}</div>
-	{/each}
-</div>
+<header />
 
 <style>
 	.splashscreen {
-		height: 100%;
+		height: 100vh;
 		width: 100%;
-		position: fixed;
+		position: absolute;
 		overflow: hidden;
-		overflow-scrolling: unset;
 		top: 0;
 		left: 0;
 		background-image: url('/static/icons/icon-512x512.png');
 		background-repeat: no-repeat;
 		background-position: center;
 		background-size: contain;
-		background-color: #068073;
+		background-color: var(--color-teal);
+		touch-action: none;
+	}
+
+	.header {
+		margin-top: env(safe-area-inset-top);
+	}
+	h1 {
+		font-size: 2em;
+		font-weight: bold;
+		color: var(--color-teal);
+		text-align: center;
+		padding: 20px;
 	}
 
 	.count-display {
@@ -105,8 +117,9 @@
 
 	.list-element {
 		font-size: 1em;
-		margin: 14px;
+		margin: 14px 0;
 		padding: 14px;
-		background-color: #19d5c6;
+		background-color: var(--color-teal);
+		color: var(--color-white);
 	}
 </style>
