b0755eba96939ff63352ee3baefa28a815ea54ea
Johannes Drechsler <JohannesDrechslerUS@gmail.com>
2022-01-29 23:39:15 -0500

    - Added initial index view :ok_hand:

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { count } from '../stores/app-store';
 	let firstName = '';
+	let gender = 'male';
 	$: fieldsetDisabled = firstName.length === 0;
 
 	function increment() {
@@ -15,9 +16,14 @@
 			return n;
 		});
 	}
+
+	async function requestFullscreen() {
+		await document.documentElement.requestFullscreen();
+	}
 </script>
 
 <h1>Welcome to Activity</h1>
+<button on:click={requestFullscreen}>Play in fullscreen</button>
 
 <p>
 	This is using the game principles of the original Activity board game made in Germany. All credits
@@ -25,7 +31,7 @@
 </p>
 
 {#if firstName.length > 0}
-	<h3>Hello {firstName}!</h3>
+	<h2>Hello {firstName}! {gender === 'male' ? '👨🏼' : '👩🏼'}</h2>
 {:else}
 	<h3>Hello stranger! What is your name?</h3>
 {/if}
@@ -39,9 +45,26 @@
 	<div class="count-display">Number: {$count}</div>
 </fieldset>
 
+<fieldset class="button-fieldset" disabled={fieldsetDisabled}>
+	<input class="radio-button" type="radio" bind:group={gender} value="male" id="male" />
+	<label for="male">👨🏼 Male</label>
+	<input class="radio-button" type="radio" bind:group={gender} value="female" id="female" />
+	<label for="female">👩🏼 Female</label>
+</fieldset>
+
 <style>
 	.count-display {
 		font-size: 1em;
 		margin: 14px;
+	}
+
+	label {
+		margin: 10px;
+		font-size: 20px;
+	}
+
+	.radio-button {
+		margin: 10px 0;
+		font-size: 23px;
 	}
 </style>
